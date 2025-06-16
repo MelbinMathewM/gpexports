3148d3554b7185a3eeba796b87017fa12e407ab7
Melbin Mathew <melbinppmathewp@gmail.com>
2025-06-16 14:03:58 +0530

    second commit

--- exports/settings.py
+++ exports/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'exports.urls'
@@ -129,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'admin_app/static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
