766706330404f7969af1734defa70dea735c5f4c
Melbin Mathew <melbinppmathewp@gmail.com>
2025-06-18 10:33:41 +0530

    added the quote form to save in database

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import quote
 
 # Register your models here.
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'email', 'phone', 'message')
+
+admin.site.register(quote, QuoteAdmin)
